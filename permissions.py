d587075de09aa8a9fe504f3cf014ad859932db5f
Ganesh Kumar S M <ksganesh18@gmail.com>
2026-03-17 10:59:26 +0530

    added debug to app, group_id

--- permissions.py
+++ permissions.py
@@ -1,6 +1,6 @@
 GROUP_VM_PERMISSIONS = {
 
-    "AI-Infra-Operators": [
+    "b7dcea9c-bdab-4352-84f3-cb5a7f8aa456": [
         "teams",
         "vm-test-01"
     ],
@@ -14,11 +14,11 @@ GROUP_VM_PERMISSIONS = {
 
 def is_vm_allowed(vm_name, groups):
 
-    for g in groups:
+    for group_id in groups:
 
-        if g in GROUP_VM_PERMISSIONS:
+        if group_id in GROUP_VM_PERMISSIONS:
 
-            if vm_name in GROUP_VM_PERMISSIONS[g]:
+            if vm_name in GROUP_VM_PERMISSIONS[group_id]:
                 return True
 
     return False
